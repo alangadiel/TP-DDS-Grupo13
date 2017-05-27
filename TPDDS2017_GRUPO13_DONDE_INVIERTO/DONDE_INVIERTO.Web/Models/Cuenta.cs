1fa7a250b7ad5f3c28130a84137a8ddace3c1a30
Adriano Alianiello <adriano.alianiello@gmail.com>
2017-05-27 20:27:50 -0300

    Models y Layout

---
+++ TPDDS2017_GRUPO13_DONDE_INVIERTO/DONDE_INVIERTO.Web/Models/Cuenta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DONDE_INVIERTO.Web.Models
+{
+    public class Cuenta
+    {
+        public int id { get; set; }
+        public string contenido { get; set; }
+    }
+}
