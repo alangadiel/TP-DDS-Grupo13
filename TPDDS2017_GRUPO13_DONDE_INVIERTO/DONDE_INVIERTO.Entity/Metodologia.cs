2aa4c06cd6842bd44743c0426c45b4e28c6d6983
danilobecca <el_beca473@hotmail.com>
2017-10-10 18:01:13 -0300

    Agrego Entidad Metodologia

---
+++ TPDDS2017_GRUPO13_DONDE_INVIERTO/DONDE_INVIERTO.Entity/Metodologia.cs
@@ -0,0 +1,29 @@
+﻿namespace DONDE_INVIERTO.Entity
+{
+    public class Metodologia : Entity<Model.Metodologia>
+    {
+        private static volatile Metodologia instance;
+        private static object syncRoot = new object();
+
+        private Metodologia()
+        {
+            base.Initialize();
+        }
+
+        public static Metodologia Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Metodologia();
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
