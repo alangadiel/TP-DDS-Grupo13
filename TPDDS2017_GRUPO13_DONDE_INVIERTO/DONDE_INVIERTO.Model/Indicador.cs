b3d6d82484e2f8e67c305ec2439d924564aeec45
danilobecca <el_beca473@hotmail.com>
2018-02-08 18:28:07 -0300

    Agrego Modelo.Indicador

--- TPDDS2017_GRUPO13_DONDE_INVIERTO/DONDE_INVIERTO.Model/Indicador.cs
+++ TPDDS2017_GRUPO13_DONDE_INVIERTO/DONDE_INVIERTO.Model/Indicador.cs
@@ -1,30 +1,57 @@
-﻿using System.Runtime.Serialization;
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using ANTLR;
 
 namespace DONDE_INVIERTO.Model
 {
-    [DataContract]
-    public class Indicador
+    public class Indicador : ComponenteOperando
     {
-        [DataMember]
-        public int Id { get; set; }
-
-        [DataMember]
-        public string Nombre { get; set; }
-
-        [DataMember]
-        public decimal? Valor { get; set; }
-
-        [DataMember]
+        const string pattern = @"((\b([A-z]*[0-9]*|[0-9]*[A-z]*)[a-z0-9]*\b)([+\-\*\/]\(?(\b([a-z]*[0-9]*|[0-9]*[a-z]*)[a-z0-9]*\b)\)?)+)";
         public string Formula { get; set; }
+        public string UsuarioCreador_Id { get; set; }
+        //public virtual ApplicationUser UsuarioCreador { get; set; }
+        public List<ComponenteOperando> Operandos { get; set; }
+        public override double ObtenerValor(Empresa empresa, int periodo, List<ComponenteOperando> listaOperandos)
+        {
+            AntlrInputStream input = new AntlrInputStream(this.Formula);
+            gramaticaLexer lexer = new gramaticaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            gramaticaParser parser = new gramaticaParser(tokens);
+            IParseTree tree = parser.expr();
+            MyVisitor visitor = new MyVisitor(empresa, periodo, listaOperandos);
+            return Math.Round(visitor.Visit(tree), 2);
+        }
+        public void Editar(Indicador indEdited)
+        {
+            //this.Nombre = indEdited.Nombre;
+            this.Formula = indEdited.Formula;
+            this.UsuarioCreador_Id = indEdited.UsuarioCreador_Id;
+            this.Operandos = indEdited.Operandos;
+        }
+        public void ValidarExpresionFormula(IEnumerable<Indicador> indicadores)
+        {
+            //GramaticaLexer lexer = new GramaticaLexer(new AntlrInputStream(this.Formula));
+            //CommonTokenStream tokens = new CommonTokenStream(lexer);
+            //GramaticaParser parser = new GramaticaParser(tokens);
+            //parser.BuildParseTree = true;
+            //GramaticaParser.IndicadorContext indicadorContext = parser.indicador();
+            //ParseTreeWalker walker = new ParseTreeWalker();
+            //Listener listener = new Listener(textBox2.Text);
+            //walker.Walk(listener, indicadorContext);
 
-        [DataMember]
-        public string Discriminador { get; set; }
+            ////lo muestro en el tree view
+            //treeView1.Nodes.Clear();
+            //BinaryTreeNode<IContenidoNodo> nodo = listener.ArbolBinario.Root;
+            //treeView1.Nodes.Add(agregarAlTreeView(nodo));
 
-        [DataMember]
-        public int? IndicadorPadreId { get; set; }
-
-        [DataMember]
-        public int? BalanceId { get; set; }
-
+            ////guardar Listener.ArbolBinario
+            //Indicadores.listaDeIndicadores.Add(listener.ArbolBinario);
+        }
     }
 }
